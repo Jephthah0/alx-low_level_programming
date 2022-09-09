63fa8081d0c8cbaf4a24efaabfa9d6a78e8ec049
Ndubuisi Jephthah <jephthahndubuisi0@gmail.com>
2022-09-09 07:54:43 -0700

    integer

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int integer;
+
+	for (integer = 0; integer < 10; integer++)
+		putchar(integer);
+	putchar('\n');
+	return (0);
+}
