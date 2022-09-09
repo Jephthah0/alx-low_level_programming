24f633679a825ae6ab016f4720aaf72429ce27c1
Ndubuisi Jephthah <jephthahndubuisi0@gmail.com>
2022-09-09 08:24:37 -0700

    Base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+	char d;
+
+	for (c = 0; c < 10; c++)
+		putchar((c % 10) + '0');
+	for (d = 'a'; d ,= 'f'; d++)
+		putchar(d);
+	putchar('\n');
+	return (0);
+}
