ff4e8434a733ecbe12331437fdb3bb28bb3d3494
Ndubuisi Jephthah <jephthahndubuisi0@gmail.com>
2022-09-14 05:30:09 -0700

    Main

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-	char *a = "_putchar"
+	char a = "_putchar";
 
-		while (*a)
-		{
-			_putchar(*a);
-			a++;
-		}
+	while (a)
+	{
+		_putchar(a);
+		a++;
+	}
 	_putchar('\n');
 	return (0);
 }
