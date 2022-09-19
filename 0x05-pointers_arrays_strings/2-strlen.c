2c72899652e4865a67af4482d9207175a6026412
Ndubuisi Jephthah <jephthahndubuisi0@gmail.com>
2022-09-19 02:17:54 -0700

    First Commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - prints the actual length  of a string
+ * @s: character to print out
+ *
+ * Return: Always o.
+ */
+int _strlen(char *s)
+{
+	char *s;
+	int len;
+
+	s = "My first strlen!";
+	len = _strlen(s);
+	_putchar("%d\n", len);
+	return (0);
+}
+
