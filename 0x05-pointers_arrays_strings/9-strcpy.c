2d95ada873cc06e281088dfadc528862d2a9104d
Ndubuisi Jephthah <jephthahndubuisi0@gmail.com>
2022-09-19 06:10:10 -0700

    Pointer

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,6 +4,7 @@
  * _strcpy - Copies a string pointed to by @src
  * @dest: A buffer to copy the string to
  * @src: The source string to copy
+ * Return: A pointer to the destination string @dest
  */
 char *_strcpy(char *dest, char *src)
 {
