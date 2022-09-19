2b2fc0e91095c7df7e2dcc15101ef9b644691b0f
Ndubuisi Jephthah <jephthahndubuisi0@gmail.com>
2022-09-19 02:47:38 -0700

    Done

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *
+ * reset_to_98 - resets the pointers value to 98
+ * @n: value to reset
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - prints out inputed characters
+ * @str: string to be displayed
+ *
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	_puts("%s\n", str);
+	return (0);
+}
