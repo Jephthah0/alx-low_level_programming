c4ba52a88f1371b6c86c2f692e575456f84838a3
Ndubuisi Jephthah <jephthahndubuisi0@gmail.com>
2022-09-19 02:32:30 -0700

    My commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,18 +2,15 @@
 
 /**
  * _strlen - prints the actual length  of a string
- * @*s: character to print out
+ * @s: character to print out
  *
  * Return: Always o.
  */
 int _strlen(char *s)
 {
-	char *s;
-	int len;
+	int length = 0;
 
-	s = "My first strlen!";
-	len = _strlen(s);
-	_putchar("%d\n", len);
-	return (0);
+	while (*s++)
+		length++;
+	return (length);
 }
-
