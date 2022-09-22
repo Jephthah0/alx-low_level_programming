1fb7993c4db3ee0d4bbf3effd2d2078776b1de41
Ndubuisi Jephthah <jephthahndubuisi0@gmail.com>
2022-09-22 08:56:19 -0700

    White space

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,7 +5,7 @@
  * @dest: The buffer storing the string
  * @src: The source string
  * @n: The maximum number of bytes to be copied from src
- * 
+ *
  * Return: A pointer to the resulting string dest
  */
 char *_strncpy(char *dest, char *src, int n)
