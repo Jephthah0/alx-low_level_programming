03f34a54db9f71581504cb50b3f539e2a66a5840
Ndubuisi Jephthah <jephthahndubuisi0@gmail.com>
2022-09-22 08:01:13 -0700

    First commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares pointers to two strings
+ * @s1: A pointer to the first string to be compared
+ * @s2: A pointer to the second string to be compared
+ * Return: Positive diference, unmatched characters or negative
+ * difference unmatched characters
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*S1 - *s2);
+}
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.c"
+
+/**
+ * reverse_array - Reverses the content of an array of integers
+ * @a: integer to be reversed
+ * @n: Number of element in array
+ */
+void reverse_array(int *a, int n)
+{
+	int tmp, index;
+
+	for (index = n - 1; index >= n / 2; index--)
+	{
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
+	}
+}
