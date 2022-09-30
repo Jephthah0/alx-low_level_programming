372757cd0aff40120dc8d2492fb1bee1e0695160
Ndubuisi Jephthah <jephthahndubuisi0@gmail.com>
2022-09-30 01:49:29 -0700

    Late

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+/**
+ * File - main
+ * Desc - Prototypes to be used
+ * Author - Ndubuisi Jephthah
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+
+#endif
